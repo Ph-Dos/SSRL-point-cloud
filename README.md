2e467a998904a27d9e1c8fed3bf4d2c995ab087f
Ph-Dos <phm47860@uga.edu>
2025-02-16 13:30:06 -0500

    mesh fixes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-
 # Road Map
+
+### Virtual ENV for Linux
+* `python3 -m venv open3d_env source open3d_env/bin/activate`
+* `pip install open3d numpy opencv-python`
--- alpha-disparity.py
+++ alpha-disparity.py
@@ -1,6 +1,8 @@
 import sys
 import numpy as np
 import cv2 as cv
+import open3d as o3d
+
 
 def generateDepthMap(SHOW_DEPTH):
     imgL = cv.imread(sys.argv[1], cv.IMREAD_GRAYSCALE)
@@ -11,11 +13,8 @@ def generateDepthMap(SHOW_DEPTH):
         blockSize = 9,
     )
     disparityMap = stereo.compute(imgL,imgR)
-    disparityMap = np.float32(disparityMap)
     disparityMap = cv.normalize(disparityMap, disparityMap, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
     disparityMap = np.uint8(disparityMap)
-    disparityMap = cv.medianBlur(disparityMap, 3)
-
     if SHOW_DEPTH:
         cv.imshow('Disparity Map', disparityMap)
         cv.waitKey(0)
@@ -23,63 +22,41 @@ def generateDepthMap(SHOW_DEPTH):
     return disparityMap
 
 
-def generatePointCloud(disparityMap):
-    disparityMap = np.float32(np.divide(disparityMap, 16.0))
+def generatePointCloud(disparityMap, fx, cx, cy, doffs, baseline):
+    disparityMap = disparityMap.astype(np.float32)
+    disparityMap[disparityMap == 0] = np.nan  # Avoid division by zero
+    disparityMap /= 16.0  # Scale factor (if disparity is stored as fixed-point)
 
-    Q = generateQMatrix()
-
-    projection = cv.reprojectImageTo3D(disparityMap, Q, handleMissingValues = False)
-
-    imgL = cv.imread(sys.argv[1])
-
-    colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
-    maskMap = disparityMap > disparityMap.min()
-    outputPoints = projection[maskMap]
-    outputColors = colors[maskMap]
-    pointCloudFile(outputPoints, outputColors)
-
-
-def generateQMatrix():
-    calibData = {}
-    with open(sys.argv[3], 'r') as file:
-        for line in file:
-            key, value = line.strip().split('=')
-            calibData[key] = value
-    P1 = np.array(list(map(float, calibData['cam0'].split()))).reshape(3, 3)
-    P2 = np.array(list(map(float, calibData['cam1'].split()))).reshape(3, 3)
+    # Compute 3D coordinates
+    h, w = disparityMap.shape
     Q = np.array([
-        [1, 0, 0, -P1[0, 2]],
-        [0, 1, 0, -P1[1, 2]],
-        [0, 0, 0, -P1[0, 0]],
-        [0, 0, -1/float(calibData['baseline']), 0]
+        [1, 0, 0, -cx],
+        [0, 1, 0, -cy],
+        [0, 0, 0, fx],
+        [0, 0, -1 / baseline, doffs / baseline]
     ])
-    return Q
 
+    # Reproject to 3D space
+    points_3D = cv.reprojectImageTo3D(disparityMap, Q)
 
-def pointCloudFile(vertices, colors):
-    colors = colors.reshape(-1, 3)
-    vertices = np.hstack([vertices.reshape(-1, 3), colors])
-
-
-    ply_header = '''ply
-        format ascii 1.0
-        element vertex {vertex_count}
-        property float x
-        property float y
-        property float z
-        property uchar red
-        property uchar green
-        property uchar blue
-        end_header
-        '''
-
-    with open('out-put.ply', 'w') as f:
-        f.write(ply_header.format(vertex_count=len(vertices)))
-        np.savetxt(f, vertices, fmt = '%f %f %f %d %d %d')
+    # Mask out invalid points
+    mask = (disparityMap > 0) & np.isfinite(points_3D[:,:,2])
+    return points_3D[mask]
 
 
 def main():
-    disparityMap = generateDepthMap(SHOW_DEPTH = False)
-    generatePointCloud(disparityMap)
+    fx = 999.421
+    cx = 294.182
+    cy = 252.932
+    doffs = 32.778
+    baseline = 193.001
+
+    disparityMap = generateDepthMap(SHOW_DEPTH = True)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(generatePointCloud(disparityMap, fx, cx, cy, doffs, baseline))
+
+    # Save to PLY file
+    o3d.io.write_point_cloud("out.ply", pcd)
+
 
 main()
